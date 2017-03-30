36db786643dc1100b770cd1f3043e31b854ed8f6
Unknown <srl447@nyu.edu>
2017-03-30 16:29:19 -0400

    Basic Edge Script
    Uses triggers and displays death message to console log

---
+++ Assets/Scripts/EdgeDeath.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EdgeDeath : MonoBehaviour {
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.tag == "Edge")
+        {
+            Debug.Log("You Died");
+        }
+    }
+}
