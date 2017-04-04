09bd8ead56bf552016fee7b5a11c9087b1af556a
Kas <ksg304@nyu.edu>
2017-04-04 14:21:38 -0400

    Imagine having hands made of nacho trees

---
+++ Assets/Scripts/JumpDark.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpDark : MonoBehaviour
+{
+    Rigidbody rbodyDarK;
+
+    public float jumpSpeed;
+
+    bool isGrounded; //result of raycast below the player
+    bool alreadyJumped = false; //has the player jumped?
+
+    float jumpAllowTimer;
+
+    void Start()
+    {
+        rbodyDarK = GetComponent<Rigidbody>();
+        jumpAllowTimer = 2f;
+
+    }
+
+
+    void Update()
+    {
+
+
+        //JUMPING
+        if (Input.GetKey(KeyCode.I))
+        {
+            if (isGrounded || jumpAllowTimer > 0)
+            {
+                rbodyDarK.transform.Translate(0f, jumpSpeed, 0f);
+                jumpAllowTimer -= Time.deltaTime;
+            }
+        }
+        else
+        {
+            jumpAllowTimer = 2f;
+        }
+
+        //if(alreadyJumped == true)
+        //{
+        //    jumpAllowTimer -= 1f;
+        //    if(jumpAllowTimer <= 0)
+        //    {
+        //        alreadyJumped = false;
+        //        jumpAllowTimer = 50f;
+        //    }
+        //}
+    }
+
+    void FixedUpdate()
+    {
+
+        //DO GROUNDED CHECK: shoot raycast just a little past bottom of capsule
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, 1.1f);
+    }
+}
