4a3b5e133464e08a8100ee0c41729d89a774abc5
Matthew Russo <mcr431@nyu.edu>
2017-04-04 14:30:20 -0400

    Added Slap Script and Detect Slap Scripts

---
+++ Assets/Scripts/DetectSlap.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectSlap : MonoBehaviour {
+
+    void OnCollisionStay(Collision col)
+    {
+        Debug.Log(name.ToString() + " GOT HIT");
+    }
+}
