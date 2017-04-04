6771c295b5bac8bdb54491fabc167f0de98c28d1
Unknown <srl447@nyu.edu>
2017-04-04 15:55:37 -0400

    Updated Walking scripts
    Also, deleted text placeholder files and added so far unused physics materials

--- Assets/Scripts/DarkWalking.cs
+++ Assets/Scripts/DarkWalking.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class DarkWalking : MonoBehaviour {
     public float speed;
+    bool leftmove;
+    bool rightmove;
     // Use this for initialization
     void Start()
     {
@@ -15,10 +17,27 @@ public class DarkWalking : MonoBehaviour {
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
+            leftmove = true;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rightmove = true;
+        }
+        else
+        {
+            leftmove = false;
+            rightmove = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (leftmove)
+        {
             transform.Translate(Vector3.left * Time.deltaTime * speed, Space.World);
             transform.eulerAngles = new Vector3(0f, 0f, 10f);
         }
-        else if (Input.GetKey(KeyCode.RightArrow))
+        else if (rightmove)
         {
             transform.Translate(Vector3.right * Time.deltaTime * speed, Space.World);
             transform.eulerAngles = new Vector3(0f, 0f, -10f);
--- Assets/Scripts/Walking.cs
+++ Assets/Scripts/Walking.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Walking : MonoBehaviour {
     public float speed;
+    bool leftmove;
+    bool rightmove;
 	// Use this for initialization
 	void Start () {
 		
@@ -14,10 +16,27 @@ public class Walking : MonoBehaviour {
     {
         if (Input.GetKey(KeyCode.A))
         {
+            leftmove = true;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rightmove = true;
+        }
+        else
+        {
+            leftmove = false;
+            rightmove = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (leftmove)
+        {
             transform.Translate(Vector3.left * Time.deltaTime * speed, Space.World);
             transform.eulerAngles = new Vector3(0f, 0f, 10f);
         }
-        else if (Input.GetKey(KeyCode.D))
+        else if (rightmove)
         {
             transform.Translate(Vector3.right * Time.deltaTime * speed, Space.World);
             transform.eulerAngles = new Vector3(0f, 0f, -10f);
