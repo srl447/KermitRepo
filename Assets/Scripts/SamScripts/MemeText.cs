0c5963584d57a94f09e1fc4b38edee1994cc40a1
Unknown <srl447@nyu.edu>
2017-04-06 14:59:46 -0400

    Updated MemeText Scripts to go with Global Round Count

--- Assets/Scripts/SamScripts/MemeText.cs
+++ Assets/Scripts/SamScripts/MemeText.cs
@@ -3,31 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MemeTextDark : MonoBehaviour
+public class MemeText : MonoBehaviour
 {
-
     string text1, text2, text3, text4, text5; //creates strings for all the text
     string[] allMemeText; //creates an array to store the text in
-    int round; //Needs to be set to the global round counters round
+    int round = GameManager.Instance.RoundCount; //Needs to be set to the global round counters round
     Text memeText; //The text to edit
-                   // Use this for initialization
-    void Start()
+	// Use this for initialization
+	void Start ()
     {
         //Sets all the strings to the meme text
-        text1 = "Stare at your phone in bed for an hour";
-        text2 = "Now spend it all";
-        text3 = "Go buy an entire pizza you earned it";
-        text4 = "OVERREACT";
-        text5 = "Stay up till 4am";
+        text1 = "Well I just woke up. Time to be productive";
+        text2 = "I'm so happy that I'm saving money";
+        text3 = "I just finished a workout, time to eat something healthy";
+        text4 = "I'm sure there's a logical explanation to this";
+        text5 = "I should really go to bed";
 
 
         memeText = GetComponent<Text>(); //assigns the text component to the text thingy
         allMemeText = new string[] { text1, text2, text3, text4, text5 }; //puts all the strings into the array
     }
-
-    // Update is called once per frame
-    void Update()
+	
+	// Update is called once per frame
+	void Update ()
     {
         memeText.text = allMemeText[round]; //changes the text to match the meme text corresponding with the current round
-    }
+	}
 }
