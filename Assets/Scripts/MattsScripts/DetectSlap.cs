9b56877873745412e7a91c327dac5639a1de9178
Unknown <srl447@nyu.edu>
2017-04-13 15:28:51 -0400

    Camera Rotate Script updated

--- Assets/Scripts/MattsScripts/DetectSlap.cs
+++ Assets/Scripts/MattsScripts/DetectSlap.cs
@@ -10,66 +10,44 @@ public class DetectSlap : MonoBehaviour
 
     public Vector3 startPos;
     public Vector3 startRot;
-    Vector3 cameraStartPos;
-    Vector3 cameraStartRot;
-    bool rotateCamera;
+
     public DetectSlap otherPlayer;
-    int timer;
 
     public void Awake()
     {
         startPos = transform.position;
         startRot = transform.eulerAngles;
-        cameraStartPos = Camera.main.transform.position;
-        cameraStartRot = Camera.main.transform.eulerAngles;
+
         otherPlayer = GameObject.FindGameObjectWithTag(tag == "kermit" ? "darkKermit" : "kermit").GetComponent<DetectSlap>();
     }
 
     void OnCollisionStay(Collision col)
     {
-        if (col.gameObject.tag == "slap") //Now this just starts my script and freezes time
+        if (col.gameObject.tag == "slap")
         {
-            Time.timeScale = 0;
-            CameraRotate();
+            Color colorToChangeTo = name == "kermitM" ? ui_roundsScript.darkKermitWon : ui_roundsScript.kermitWon;
+
+            ui_roundsScript.ChangeLight(GameManager.Instance.RoundCount, colorToChangeTo);
+
+            if (GameManager.Instance.RoundCount >= 4)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                return;
+            }
+            GameManager.Instance.RoundCount++;
+
+            transform.position = startPos;
+            transform.eulerAngles = startRot;
+            otherPlayer.transform.position = otherPlayer.startPos;
+            otherPlayer.transform.eulerAngles = otherPlayer.startRot;
         }
-    }
-
-    void CameraRotate() //initiates the rotating camera process
-    { 
-        //moves the camera to the position needed to rotate
-        Camera.main.transform.position = new Vector3(((transform.position.x + otherPlayer.transform.position.x) / 2) - 3, (transform.position.x + otherPlayer.transform.position.x) / 3, cameraStartPos.z + 3);
-        Camera.main.transform.eulerAngles = new Vector3(11f, 45f, 0f);
-        //changes the variable so code can run in update
-        rotateCamera = true;
-    }
-
-    void resetWorld() //moved Matt's code here so I could execute it after rotation
-    {
-        Color colorToChangeTo = tag == "kermit" ? ui_roundsScript.darkKermitWon : ui_roundsScript.kermitWon;
-
-        ui_roundsScript.ChangeLight(GameManager.Instance.RoundCount, colorToChangeTo);
-
-        //resetting my variables and timescale
-        Time.timeScale = 1;
-        timer = 0;
-        rotateCamera = false;
-
-        if (GameManager.Instance.RoundCount >= 4)
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-            return;
-        }
-        GameManager.Instance.RoundCount++;
-
-        transform.position = startPos;
-        transform.eulerAngles = startRot;
-        Camera.main.transform.position = cameraStartPos;
-        Camera.main.transform.eulerAngles = cameraStartRot;
-        otherPlayer.transform.position = otherPlayer.startPos;
-        otherPlayer.transform.eulerAngles = otherPlayer.startRot;
     }
     void Update()
     {
+        if(!rotateCamera)
+        {
+            Camera.main.transform.position = new Vector3(((transform.position.x + otherPlayer.transform.position.x) / 2), Camera.main.transform.position.y, Camera.main.transform.position.z);
+        }
         if (rotateCamera) //rotates the camera
         {
             Camera.main.transform.eulerAngles += new Vector3(0f, -1f, 0f);
