325c0f945b399be81ec6a352414375bdc39c85cd
Matthew Russo <mcr431@nyu.edu>
2017-05-09 14:37:03 -0400

    Hit Particles larger, Countdown stops movement, makes countdown a prefab

--- Assets/Scripts/MattsScripts/UI/IntroCountDown.cs
+++ Assets/Scripts/MattsScripts/UI/IntroCountDown.cs
@@ -12,6 +12,15 @@ public class IntroCountDown : MonoBehaviour
 
     public Image countdownImage;
 
+    public Walking kermitWalk;
+    public Walking darkKermitWalk;
+    public Slap kermitSlap;
+    public Slap darkKermitSlap;
+    public Jump kermitJump;
+    public Jump darkKermitJump;
+    public Block kermitBlock;
+    public DarkBlock darkKermitBlock;
+
     public void Awake()
     {
         countdownImage = GetComponent<Image>();
@@ -24,6 +33,14 @@ public class IntroCountDown : MonoBehaviour
 
     public IEnumerator Countdown()
     {
+        kermitBlock.enabled = false;
+        kermitJump.enabled = false;
+        kermitSlap.enabled = false;
+        kermitWalk.enabled = false;
+        darkKermitBlock.enabled = false;
+        darkKermitJump.enabled = false;
+        darkKermitSlap.enabled = false;
+        darkKermitWalk.enabled = false;
         //enable image
         countdownImage.enabled = true;
 
@@ -62,17 +79,27 @@ public class IntroCountDown : MonoBehaviour
         yield return new WaitForSecondsRealtime(.6f);
 
         // FIGHT
-        //countdownImage.sprite = fightSprite;
-        //countdownImage.transform.localScale = Vector3.zero;
-        //while (countdownImage.transform.localScale.x < .95)
-        //{
-        //    countdownImage.transform.localScale = Vector3.Lerp(countdownImage.transform.localScale, Vector3.one, .2f);
-        //    yield return new WaitForEndOfFrame();
-        //}
-        //countdownImage.transform.localScale = Vector3.one;
-        //yield return new WaitForSecondsRealtime(.5f);
+        countdownImage.sprite = fightSprite;
+        countdownImage.preserveAspect = true;
+        countdownImage.transform.localScale = Vector3.zero;
+        while (countdownImage.transform.localScale.x < 4.95f)
+        {
+            countdownImage.transform.localScale = Vector3.Lerp(countdownImage.transform.localScale, Vector3.one * 5f, .2f);
+            yield return new WaitForEndOfFrame();
+        }
+        countdownImage.transform.localScale = Vector3.one * 5f;
+        yield return new WaitForSecondsRealtime(.5f);
 
         // Turn off the Countdown Image
         countdownImage.enabled = false;
+
+        kermitBlock.enabled = true;
+        kermitJump.enabled = true;
+        kermitSlap.enabled = true;
+        kermitWalk.enabled = true;
+        darkKermitBlock.enabled = true;
+        darkKermitJump.enabled = true;
+        darkKermitSlap.enabled = true;
+        darkKermitWalk.enabled = true;
     }
 }
